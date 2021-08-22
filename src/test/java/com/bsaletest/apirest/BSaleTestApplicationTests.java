0ebf7cff84b3b7a574d624a9695abd933f4e8339
Azgaf012 <47955628+Azgaf012@users.noreply.github.com>
2021-08-22 16:58:39 -0500

    make it better

---
+++ src/test/java/com/bsaletest/apirest/BSaleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsaletest.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSaleTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
